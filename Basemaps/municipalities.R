6e21f17c335d095691e8ca394683f655cdc06277
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-03-17 10:47:08 +0200

    Change input data to MML data

--- Basemaps/municipalities.R
+++ Basemaps/municipalities.R
@@ -3,9 +3,30 @@ library(here)
 library(tidyverse)
 library(sf)
 
-municipalities <- here::here("Basemaps", "input", "kuntarajat.shp") %>%
-  sf::read_sf(options = "ENCODING=WINDOWS-1252") %>%
-  sf::st_set_crs(3879)
+municipalities <- here::here("Basemaps", "input", "SuomenKuntajako_2021_100k") %>%
+  sf::read_sf(options = "ENCODING=ISO-8859-1") %>%
+  sf::st_set_crs(3067) %>%
+  dplyr::mutate(across(is.character, ~iconv(.x, from = "ISO-8859-1", to = "UTF-8")))
+
+hs15 <- c("Helsinki",
+          "Espoo",
+          "Kauniainen",
+          "Vantaa",
+          "Hyvinkää",
+          "Järvenpää",
+          "Kerava",
+          "Kirkkonummi",
+          "Mäntsälä",
+          "Nurmijärvi",
+          "Pornainen",
+          "Sipoo",
+          "Tuusula",
+          "Vihti",
+          "Siuntio")
+
+municipalities <- municipalities %>%
+  dplyr::filter(namefin %in% hs15) %>%
+  sf::st_transform(crs = 3879)
 
 municipalities %>%
   readr::write_rds(here::here("Basemaps", "municipalities.rds"))
