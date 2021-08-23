4faad2211459785b933024986728a73a62836466
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-08-23 16:31:24 +0300

    Fix paths

--- scripts/figures/zones.R
+++ scripts/figures/zones.R
@@ -1,6 +1,7 @@
 # -*- coding: utf-8-unix -*-
 library(here)
 library(tidyverse)
+library(sf)
 
 read_tsv_helmet <- function(..., comment = "#") {
   readr::read_tsv(..., comment = comment) %>%
@@ -14,72 +15,68 @@ read_tsv_helmet <- function(..., comment = "#") {
 zones <- readr::read_rds(here::here("results", "zones.rds"))
 
 pop <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2017.pop"),
+  list.files(here::here("data", "Syottodata", "2020"), pattern = ".pop$", full.names = TRUE),
   col_types = "iiddddd"
 )
 lnd <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2017.lnd"),
+  list.files(here::here("data", "Syottodata", "2020"), pattern = ".lnd$", full.names = TRUE),
   col_types = "idd"
 )
 edu <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2017.edu"),
+  list.files(here::here("data", "Syottodata", "2020"), pattern = ".edu$", full.names = TRUE),
   col_types = "iiii---"
 )
-car <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2017.car"),
-  col_types = "idd"
-)
 wrk <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2016.wrk"),
+  list.files(here::here("data", "Syottodata", "2020"), pattern = ".wrk$", full.names = TRUE),
   col_types = "iidddd"
 )
 prk <- read_tsv_helmet(
-  here::here("data", "Lahtodata", "2016_zonedata", "2016.prk"),
+  list.files(here::here("data", "Syottodata", "2020"), pattern = ".prk$", full.names = TRUE),
   col_types = "iii"
 )
 
 accessibility <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "accessibility.txt"),
+  here::here("data", "Tulokset", "2020", "accessibility.txt"),
   col_types = "iddddddddddddddddddddddddddddddddddddddddddddddddd"
 )
 attraction <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "attraction.txt"),
-  col_types = "iddddddddddddd"
+  here::here("data", "Tulokset", "2020", "attraction.txt"),
+  col_types = "idddddddddddd"
 )
 car_density <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "car_density.txt"),
+  here::here("data", "Tulokset", "2020", "car_density.txt"),
   col_types = "id"
 )
 car_use <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "car_use.txt"),
+  here::here("data", "Tulokset", "2020", "car_use.txt"),
   col_types = "id"
 )
 generation <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "generation.txt"),
-  col_types = "iddddddddddddd"
+  here::here("data", "Tulokset", "2020", "generation.txt"),
+  col_types = "idddddddddddd"
 )
 impedance_ratio <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "impedance_ratio.txt"),
+  here::here("data", "Tulokset", "2020", "impedance_ratio.txt"),
   col_types = "idd"
 )
 origins_demand <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "origins_demand.txt"),
+  here::here("data", "Tulokset", "2020", "origins_demand.txt"),
   col_types = "idddd"
 )
 origins_shares <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "origins_shares.txt"),
+  here::here("data", "Tulokset", "2020", "origins_shares.txt"),
   col_types = "idddd"
 )
 savu <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "savu.txt"),
+  here::here("data", "Tulokset", "2020", "savu.txt"),
   col_types = "id"
 )
 sustainable_accessibility <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "sustainable_accessibility.txt"),
+  here::here("data", "Tulokset", "2020", "sustainable_accessibility.txt"),
   col_types = "iddddddddddd"
 )
 workforce_accessibility <- read_tsv_helmet(
-  here::here("data", "helmet_4.0.4_2018_results", "workforce_accessibility.txt"),
+  here::here("data", "Tulokset", "2020", "workforce_accessibility.txt"),
   col_types = "id"
 )
 
@@ -104,7 +101,6 @@ zones <- zones %>%
   dplyr::left_join(pop, by = "zone") %>%
   dplyr::left_join(lnd, by = "zone") %>%
   dplyr::left_join(edu, by = "zone") %>%
-  dplyr::left_join(car, by = "zone") %>%
   dplyr::left_join(wrk, by = "zone") %>%
   dplyr::left_join(prk, by = "zone") %>%
   dplyr::left_join(savu, by = "zone") %>%
