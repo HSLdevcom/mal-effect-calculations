228a0dfb2637d25d70d4d4e91edfced384eff082
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-08-30 11:21:23 +0300

    Update run procedure and rename scenarios

--- scripts/figures/run.R
+++ scripts/figures/run.R
@@ -13,29 +13,20 @@ files_in <- list.files(here::here("scripts", "figures"),
                        pattern = "^map.*R$",
                        full.names = TRUE)
 
-Sys.setenv(R_CONFIG_ACTIVE = "2020")
-source(here::here("scripts", "figures", "zones.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "areas.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "vdfs.R"), encoding = "utf-8")
-# lapply(files_in, verbose_source, encoding = "utf-8")
-
-Sys.setenv(R_CONFIG_ACTIVE = "2040_ve0")
-source(here::here("scripts", "figures", "zones.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "areas.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "vdfs.R"), encoding = "utf-8")
-# lapply(files_in, verbose_source, encoding = "utf-8")
-
-Sys.setenv(R_CONFIG_ACTIVE = "2040_ve0_muulitar")
-source(here::here("scripts", "figures", "zones.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "areas.R"), encoding = "utf-8")
-source(here::here("scripts", "figures", "vdfs.R"), encoding = "utf-8")
-# lapply(files_in, verbose_source, encoding = "utf-8")
+for (scenario in c("2020", "2040_ve0", "2040_ve0_muulitar")) {
+  Sys.setenv(R_CONFIG_ACTIVE = scenario)
+  source(here::here("scripts", "figures", "zones.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "areas.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "vdfs.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "graph_centers.R"), encoding = "utf-8")
+  lapply(files_in, verbose_source, encoding = "utf-8")
+}
 
 Sys.setenv(R_CONFIG_ACTIVE = "default")
 
 areas_all <- dplyr::bind_rows(
-  "2020 Nyky" = readr::read_rds(here::here("results", "areas_2020.rds")),
-  "2040 Pohja" = readr::read_rds(here::here("results", "areas_2040_ve0.rds")),
+  "2020 Nykytila" = readr::read_rds(here::here("results", "areas_2020.rds")),
+  "2040 Vertailupohja" = readr::read_rds(here::here("results", "areas_2040_ve0.rds")),
   "2040 Etätyö+" = readr::read_rds(here::here("results", "areas_2040_ve0_muulitar.rds")),
   .id = "scenario") %>%
   dplyr::mutate(scenario = forcats::as_factor(scenario))
@@ -43,8 +34,8 @@ areas_all <- dplyr::bind_rows(
 readr::write_rds(areas_all, file = here::here("results", "areas_all.rds"))
 
 vdfs_all <- dplyr::bind_rows(
-  "2020 Nyky" = readr::read_rds(here::here("results", "vdfs_2020.rds")),
-  "2040 Pohja" = readr::read_rds(here::here("results", "vdfs_2040_ve0.rds")),
+  "2020 Nykytila" = readr::read_rds(here::here("results", "vdfs_2020.rds")),
+  "2040 Vertailupohja" = readr::read_rds(here::here("results", "vdfs_2040_ve0.rds")),
   "2040 Etätyö+" = readr::read_rds(here::here("results", "vdfs_2040_ve0_muulitar.rds")),
   .id = "scenario") %>%
   dplyr::mutate(scenario = forcats::as_factor(scenario))
