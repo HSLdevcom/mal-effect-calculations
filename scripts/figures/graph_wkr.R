0f1d447848825876ac0cfcad155921b8402d39b7
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-08-30 16:39:16 +0300

    Plot population and workplaces

---
+++ scripts/figures/graph_wkr.R
@@ -0,0 +1,34 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", "areas_all.rds"))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(results, aes(x = scenario, y = total_wrk)) +
+  geom_col(aes(fill = area), position = position_stack()) +
+  scale_y_continuous(
+    labels = scales::label_number()
+  ) +
+  scale_x_discrete(
+    labels = scales::label_wrap(5)
+  ) +
+  scale_fill_manual(
+    name = NULL,
+    values = c("#3E8606", "#BFD7AC", "#f092cd", "#007AC9", "#AAD3ED")
+  ) +
+  labs(
+    title = "Työpaikat",
+    x =  NULL,
+    y = NULL
+  ) +
+  theme_mal_graph() +
+  theme(legend.position = "right")
+
+ggsave_graph(here::here("figures", "graph_wkr.png"))
