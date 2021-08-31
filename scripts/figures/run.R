8b48232dc85af49ebae1ee968e18169e7d1c7a0e
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-08-31 17:14:02 +0300

    Update run job
    Made it lengthier because some plots are scenario-dependent
    and some are not and a simple "graph" vs "map" separation will not
    do anymore. Plot naming conventions need to be revisited later...

--- scripts/figures/run.R
+++ scripts/figures/run.R
@@ -8,24 +8,28 @@ verbose_source <- function(file, ...) {
   invisible(source(file, ...))
 }
 
-# When running maps and graphs, the order of the files does not matter.
-files_in <- list.files(here::here("scripts", "figures"),
-                       pattern = "^map.*R$",
-                       full.names = TRUE)
+source(here::here("scripts", "figures", "map_centers.R"), encoding = "utf-8")
 
-for (scenario in c("2020", "2040_ve0", "2040_ve0_muulitar")) {
+for (scenario in c("2018", "2040_ve0", "2040_ve0_muulitar")) {
   Sys.setenv(R_CONFIG_ACTIVE = scenario)
   source(here::here("scripts", "figures", "zones.R"), encoding = "utf-8")
   source(here::here("scripts", "figures", "areas.R"), encoding = "utf-8")
   source(here::here("scripts", "figures", "vdfs.R"), encoding = "utf-8")
+
   source(here::here("scripts", "figures", "graph_centers.R"), encoding = "utf-8")
-  lapply(files_in, verbose_source, encoding = "utf-8")
+
+  source(here::here("scripts", "figures", "map_car-density.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "map_mode-share_bike-walk.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "map_mode-share_sustainable.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "map_savu.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "map_savu_workplaces.R"), encoding = "utf-8")
+  source(here::here("scripts", "figures", "map_workforce-accessibility.R"), encoding = "utf-8")
 }
 
 Sys.setenv(R_CONFIG_ACTIVE = "default")
 
 areas_all <- dplyr::bind_rows(
-  "2020 Nykytila" = readr::read_rds(here::here("results", "areas_2020.rds")),
+  "2018 Nykytila" = readr::read_rds(here::here("results", "areas_2018.rds")),
   "2040 Vertailupohja" = readr::read_rds(here::here("results", "areas_2040_ve0.rds")),
   "2040 Etätyö+" = readr::read_rds(here::here("results", "areas_2040_ve0_muulitar.rds")),
   .id = "scenario") %>%
@@ -34,7 +38,7 @@ areas_all <- dplyr::bind_rows(
 readr::write_rds(areas_all, file = here::here("results", "areas_all.rds"))
 
 vdfs_all <- dplyr::bind_rows(
-  "2020 Nykytila" = readr::read_rds(here::here("results", "vdfs_2020.rds")),
+  "2018 Nykytila" = readr::read_rds(here::here("results", "vdfs_2018.rds")),
   "2040 Vertailupohja" = readr::read_rds(here::here("results", "vdfs_2040_ve0.rds")),
   "2040 Etätyö+" = readr::read_rds(here::here("results", "vdfs_2040_ve0_muulitar.rds")),
   .id = "scenario") %>%
@@ -43,7 +47,14 @@ vdfs_all <- dplyr::bind_rows(
 readr::write_rds(vdfs_all, file = here::here("results", "vdfs_all.rds"))
 
 # When running maps and graphs, the order of the files does not matter.
-# files_in <- list.files(here::here("scripts", "figures"),
-#                        pattern = "^graph.*R$",
-#                        full.names = TRUE)
-# lapply(files_in, verbose_source, encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_car_density.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_co2.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_mode-share_sustainable.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_pop.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_savu_workplaces.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_vehicle-kms-vdf.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_vehicle_kms.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_wrk.R"), encoding = "utf-8")
+source(here::here("scripts", "figures", "graph_workforce-accessibility.R"), encoding = "utf-8")
+
+source(here::here("scripts", "figures", "map_diff_mode-share_sustainable.R"), encoding = "utf-8")
