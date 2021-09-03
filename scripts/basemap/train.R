fe30aa0b9594a693c7edf1810c4495be9d9c2f1e
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-09-03 16:20:30 +0300

    Update basemap data

--- scripts/basemap/train.R
+++ scripts/basemap/train.R
@@ -3,16 +3,8 @@ library(here)
 library(tidyverse)
 library(sf)
 
-train1 <- here::here("data", "keharata.tab") %>%
-  sf::read_sf(options = "ENCODING=WINDOWS-1252") %>%
-  sf::st_set_crs(3879)
-
-train2 <- here::here("data", "juna_leik.shp") %>%
-  sf::read_sf(options = "ENCODING=WINDOWS-1252") %>%
-  sf::st_set_crs(3879)
-
-train <- dplyr::bind_rows(train1, train2) %>%
-  sf::st_union()
+train <- here::here("data", "RautatieViiva_Maastokartta100k_MML_Henkilorata_HS15.gpkg") %>%
+  sf::read_sf()
 
 train %>%
   readr::write_rds(here::here("results", "train.rds"))
