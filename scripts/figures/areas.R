02255e58638821743598a30d9ed93e8b9a26fcc3
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-09-27 09:42:56 +0300

    Fix an error in truck emissions

--- scripts/figures/areas.R
+++ scripts/figures/areas.R
@@ -181,7 +181,7 @@ areas <- areas %>%
     co2_van = co2["van"] * sum(.$vehicle_kms_van),
     co2_bus_hsl = co2["bus_hsl"] * vehicle_kms_bus_hsl,
     co2_bus_other = co2["bus_other"] * vehicle_kms_bus_other,
-    co2_truck_all = co2["truck_all"] * sum(.$vehicle_kms_trailer_truck) + sum(.$vehicle_kms_trailer_truck),
+    co2_truck_all = co2["truck_all"] * (sum(.$vehicle_kms_trailer_truck) + sum(.$vehicle_kms_truck)),
     sustainable_accessibility = weighted.mean(.$sustainable_accessibility, .$total_pop),
     workforce_accessibility = weighted.mean(.$workforce_accessibility, .$total_wrk),
     car_density = weighted.mean(.$car_density, .$total_pop),
