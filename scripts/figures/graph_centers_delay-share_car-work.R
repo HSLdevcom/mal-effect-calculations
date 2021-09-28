8f9b594a62a9b16b3383ae484479d9090f6c5e9d
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-09-28 11:29:32 +0300

    Add graphs about travel times between centers

---
+++ scripts/figures/graph_centers_delay-share_car-work.R
@@ -0,0 +1,40 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(omxr)
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", sprintf("centers_%s.rds", config::get("scenario"))))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(data = results) +
+  geom_raster(mapping = aes(x = destination,
+                            y = forcats::fct_rev(origin),
+                            fill = delay_share_car_work)) +
+  scale_x_discrete(position = "top",
+                   guide = guide_axis(angle = 90)) +
+  scale_fill_fermenter(
+    name = NULL,
+    type = "div",
+    palette = "PuOr",
+    breaks = seq(-0.35, 0.35, 0.1),
+    limits = c(-0.35, 0.35),
+    na.value = "#FFFFFF",
+    labels = scales::percent_format(accuracy = 1, suffix = " %")
+  ) +
+  labs(
+    title = "Ruuhkaviiveen osuus henkilöautoliikenteen matka-ajasta aamuhuipputuntina",
+    subtitle = sprintf("%d %s", config::get("year"), config::get("scenario_name")),
+    x = "Määräpaikka",
+    y = "Lähtöpaikka"
+  ) +
+  coord_equal() +
+  theme_mal_graph() +
+  theme(legend.position = "right",
+        panel.grid.major.y = element_blank())
+
+ggsave_graph(here::here("figures", sprintf("graph_centers-delay_share-car_work_%s.png", config::get("scenario"))))
--- scripts/figures/graph_centers_ttime-ratio_aht.R
+++ scripts/figures/graph_centers_ttime-ratio_aht.R
@@ -3,27 +3,18 @@ library(here)
 library(tidyverse)
 library(omxr)
 
-centers <- readr::read_tsv(here::here("data", "centers.tsv"), col_types = "ic")
 
-ttimes <- read_helmet_omx(file.path(config::get("helmet_data"),
-                                    config::get("results"),
-                                    "Matrices",
-                                    "time_aht.omx")) %>%
-  dplyr::select(origin, destination, car_work, transit_work) %>%
-  dplyr::filter(origin %in% centers$level & destination %in% centers$level) %>%
-  dplyr::mutate(
-    origin = factor(origin, levels = centers$level, labels = centers$label),
-    destination = factor(destination, levels = centers$level, labels = centers$label),
-    ttime_ratio = transit_work / car_work
-  )
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", sprintf("centers_%s.rds", config::get("scenario"))))
 
 
 # Plot --------------------------------------------------------------------
 
-ggplot(data = ttimes) +
+ggplot(data = results) +
   geom_raster(mapping = aes(x = destination,
                             y = forcats::fct_rev(origin),
-                            fill = ttime_ratio)) +
+                            fill = ttime_ratio_aht)) +
   scale_x_discrete(position = "top",
                    guide = guide_axis(angle = 90)) +
   scale_fill_fermenter(
@@ -43,6 +34,7 @@ ggplot(data = ttimes) +
   ) +
   coord_equal() +
   theme_mal_graph() +
-  theme(legend.position = "right")
+  theme(legend.position = "right",
+        panel.grid.major.y = element_blank())
 
 ggsave_graph(here::here("figures", sprintf("graph_centers_%s.png", config::get("scenario"))))
