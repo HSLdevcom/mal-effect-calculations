0f409028de1a6a22527343d1ffee4b3108ef2bb6
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-09-29 14:26:17 +0300

    Update metro lines

--- scripts/basemap/metro.R
+++ scripts/basemap/metro.R
@@ -3,20 +3,8 @@ library(here)
 library(tidyverse)
 library(sf)
 
-metro1 <- here::here("data", "Länsimetron_jatke.TAB") %>%
-  sf::read_sf() %>%
-  sf::st_set_crs(3879)
-
-metro2 <- here::here("data", "Lansimetro.tab") %>%
-  sf::read_sf() %>%
-  sf::st_set_crs(3879)
-
-metro3 <- here::here("data", "metro.shp") %>%
-  sf::read_sf() %>%
-  sf::st_set_crs(3879)
-
-metro <- dplyr::bind_rows(metro1, metro2, metro3) %>%
-  sf::st_union()
+metro <- here::here("data", "RautatieViiva_Maastokartta100k_MML_Metro_HS15.gpkg") %>%
+  sf::read_sf()
 
 metro %>%
   readr::write_rds(here::here("results", "metro.rds"))
