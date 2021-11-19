8da9c10b864c979f85da05cabc844f21bb666ce1
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-11-19 17:23:37 +0200

    Handle links and add two plots

---
+++ scripts/figures/single/map_volumes-aht.R
@@ -0,0 +1,37 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+library(viridis)
+source(here::here("scripts", "basemap", "functions_map.R"), encoding = "utf-8")
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", sprintf("buffers_%s.rds", config::get("scenario")))) %>%
+  # Arrange for improved plotting
+  dplyr::arrange(volume_aht)
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot() +
+  geom_basemap() +
+  geom_sf(mapping = aes(fill = volume_aht),
+          data = results, color = NA) +
+  scale_fill_viridis_b(
+    option = "inferno",
+    name = "ajon. / h",
+    breaks = seq(1000, 5000, by = 1000),
+    limits = c(0, 6000),
+    direction = -1,
+    oob = scales::squish
+  ) +
+  coord_sf_mal() +
+  annotate_map(
+    title = "Moottoriajoneuvoliikenteen liikennemäärä aamuhuipputuntina",
+    subtitle = sprintf("%d %s", config::get("year"), config::get("scenario_name"))
+  ) +
+  theme_mal_map()
+
+ggsave_map(here::here("figures", sprintf("map_volumes-aht_%s.png", config::get("scenario"))))
