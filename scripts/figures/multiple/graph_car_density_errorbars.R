dc579838b4d6deabf0525df02a06623ecbf29137
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-12-10 18:55:09 +0200

    Read data from sensitivity scenarios and plot error bars

---
+++ scripts/figures/multiple/graph_car_density_errorbars.R
@@ -0,0 +1,46 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", "areas_all.rds"))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(results, aes(x = area, y = car_density, fill = scenario)) +
+  geom_col(position = position_dodge2()) +
+  geom_errorbar(
+    mapping = aes(ymin = car_density_lower, ymax = car_density_upper),
+    position =  position_dodge2(width = 0.5, padding = 0.5),
+    color = "#333333"
+  ) +
+  geom_text(
+    aes(y = car_density, label = scales::label_number(accuracy = 1)(car_density)),
+    position = position_dodge2(width = 0.9),
+    vjust = -0.5,
+    size = points2mm(8),
+    color = "#333333"
+  ) +
+  scale_y_continuous(
+    labels = scales::label_number(),
+    expand = expansion(mult = 0.1)
+  ) +
+  scale_x_discrete(
+    labels = scales::label_wrap(5)
+  ) +
+  scale_fill_manual(
+    name = NULL,
+    values = c("#3E8606", "#7DAD58", "#BFD7AC")
+  ) +
+  labs(
+    title = "Henkilöautotiheys",
+    x =  NULL,
+    y = "autoa / 1000 asukasta"
+  ) +
+  theme_mal_graph()
+
+ggsave_graph(here::here("figures", "graph_car_density_errorbars.png"))
