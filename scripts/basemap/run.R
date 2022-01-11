a7b9d762961488baa4f7e3f1f126a4e9012e4d15
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-01-11 16:41:47 +0200

    Move to correct place
    Needs zones to run

--- scripts/basemap/run.R
+++ scripts/basemap/run.R
@@ -1,8 +1,6 @@
 # -*- coding: utf-8-unix -*-
 library(here)
 
-source(here::here("scripts", "basemap", "centers_uusimaa-2050.R"), encoding = "utf-8")
-
 source(here::here("scripts", "basemap", "municipalities.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "bonus_region.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "region.R"), encoding = "utf-8")
@@ -13,6 +11,8 @@ source(here::here("scripts", "basemap", "roads.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "train.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "water.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "zones.R"), encoding = "utf-8")
+
+source(here::here("scripts", "basemap", "centers_uusimaa-2050.R"), encoding = "utf-8")
 
 source(here::here("scripts", "basemap", "functions_map.R"), encoding = "utf-8")
 source(here::here("scripts", "basemap", "map.R"), encoding = "utf-8")
