5753e3cdd7c3618c91703fce439fe08c44f329b3
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-02-11 11:35:49 +0200

    Read and plot ensisijaiset vyöhykkeet
    Now it is old data but it will be changed to 2023 version when released.

---
+++ scripts/basemap/ensi.R
@@ -0,0 +1,34 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+source(here::here("scripts", "basemap", "functions_map.R"), encoding = "utf-8")
+
+
+# Data --------------------------------------------------------------------
+
+ensi <- sf::read_sf(here::here("data", "ensisijaisetvyohykkeet2019",
+                               "MAL_ensisijaiset_vyöhykkeet_20190329_GK25.shp"),
+                    options = "ENCODING=UTF-8") %>%
+  sf::st_cast("POLYGON") %>%
+  sf::st_transform(3879) %>%
+  dplyr::mutate(ensi = TRUE) %>%
+  dplyr::select(ensi)
+
+readr::write_rds(ensi, file = here::here("results", "ensi.rds"))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot() +
+  geom_basemap() +
+  geom_sf(mapping = aes(),
+          data = ensi, color = NA, fill = "#3E8606", alpha = 0.5) +
+  coord_sf_mal() +
+  annotate_map(
+    title = "Ensisijaisesti kehitettävät maankäytön vyöhykkeet",
+    subtitle = sprintf("%d %s", config::get("year"), config::get("scenario_name"))
+  ) +
+  theme_mal_map()
+
+ggsave_map(here::here("figures", "map_ensi.png"))
