7429cfb3eb13116c16421b99f3a10222492856a3
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-02-16 10:53:28 +0200

    Add CBA map with the sum of car and transit

--- scripts/figures/difference/diff_map_cba.R
+++ scripts/figures/difference/diff_map_cba.R
@@ -59,3 +59,27 @@ ggplot() +
   theme_mal_map()
 
 ggsave_map(here::here("figures", sprintf("map_cba_car-time_%s.png", config::get("scenario"))))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot() +
+  geom_sf(mapping = aes(fill = cba_transit_time_per_person + cba_car_time_per_person),
+          data = results, color = NA) +
+  scale_fill_distiller(
+    palette = "PRGn",
+    direction = -1,
+    name = "minuuttia",
+    limits = c(-15, 15),
+    labels = scales::label_number(accuracy = 1),
+    oob = scales::oob_squish_any
+  ) +
+  geom_basemap() +
+  coord_sf_mal() +
+  annotate_map(
+    title = "Joukkoliikenteen ja henkilöauton yhteenlaskettu matka-aikasäästö asukasta kohden",
+    subtitle = "2040 Vertailupohja \U2192 2040 Varjo"
+  ) +
+  theme_mal_map()
+
+ggsave_map(here::here("figures", sprintf("map_cba_car-transit-time_%s.png", config::get("scenario"))))
