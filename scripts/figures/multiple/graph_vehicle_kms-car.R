d5cbcce61772fd83e872afe694ece2cd05704911
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-02-28 15:56:02 +0200

    Add error bars to vehicle kms and unify y scale

--- scripts/figures/multiple/graph_vehicle_kms-car.R
+++ scripts/figures/multiple/graph_vehicle_kms-car.R
@@ -8,46 +8,40 @@ library(sf)
 
 results <- readr::read_rds(here::here("results", "areas_all.rds"))
 
-results_total <- results %>%
-  dplyr::filter(area == "Helsingin seutu")
-
-results <- results %>%
-  dplyr::filter(area != "Helsingin seutu")
-
 
 # Plot --------------------------------------------------------------------
 
-ggplot(results, aes(x = scenario, y = vehicle_kms_car)) +
-  geom_col(aes(fill = area), position = position_stack()) +
+ggplot(results, aes(x = area, y = vehicle_kms_car, fill = scenario)) +
+  geom_col(position = position_dodge2()) +
+  geom_errorbar(
+    mapping = aes(ymin = vehicle_kms_car_lower, ymax = vehicle_kms_car_upper),
+    position =  position_dodge2(width = 0.9, padding = 0.66),
+    color = "#333333",
+    size = 0.35
+  ) +
   geom_text(
-    aes(label = scales::label_number(scale = 0.000001, accuracy = 0.1, decimal.mark = ",")(vehicle_kms_car), group = area),
-    position = position_stack(vjust = 0.5),
+    aes(y = vehicle_kms_car / 2, label = scales::label_number(scale = 0.000001, accuracy = 0.1, decimal.mark = ",")(vehicle_kms_car), group = area),
+    position = position_dodge2(width = 0.9),
     size = points2mm(8),
     color = "#333333"
   ) +
-  geom_text(data = results_total,
-            aes(label = scales::label_number(scale = 0.000001, accuracy = 0.1, decimal.mark = ",")(vehicle_kms_car)),
-            vjust = -0.5,
-            size = points2mm(8),
-            fontface = "bold",
-            color = "#333333") +
   scale_y_continuous(
     labels = scales::label_number(scale = 0.000001),
-    expand = expansion(mult = 0.1)
+    expand = expansion(mult = 0.1),
+    limits = c(0, 45000000)
   ) +
   scale_x_discrete(
     labels = scales::label_wrap(5)
   ) +
   scale_fill_manual(
     name = NULL,
-    values = c("#3E8606", "#BFD7AC", "#f092cd", "#007AC9", "#AAD3ED")
+    values = c("#3E8606", "#7DAD58", "#BFD7AC")
   ) +
   labs(
     title = "Henkilöautoliikenteen liikennesuorite Helsingin seudulla",
     x =  NULL,
     y = "milj. ajon.km / arki-vrk"
   ) +
-  theme_mal_graph() +
-  theme(legend.position = "right")
+  theme_mal_graph()
 
 ggsave_graph(here::here("figures", "graph_vehicle_kms_car.png"))
