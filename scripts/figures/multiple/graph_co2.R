a9ecb6eadf33eadfba4fef5b1210ee161a1ba144
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-03-01 14:25:06 +0200

    Add error bars

--- scripts/figures/multiple/graph_co2.R
+++ scripts/figures/multiple/graph_co2.R
@@ -13,6 +13,12 @@ results <- readr::read_rds(here::here("results", "emissions_all.rds")) %>%
 
 ggplot(results, aes(x = vehicle, y = emission, fill = scenario)) +
   geom_col(position = position_dodge2()) +
+  geom_errorbar(
+    mapping = aes(ymin = emission_lower, ymax = emission_upper),
+    position =  position_dodge2(width = 0.9, padding = 0.66),
+    color = "#333333",
+    size = 0.35
+  ) +
   geom_text(
     aes(y = emission / 2, label = scales::label_number(scale = 10^(-9), accuracy = 1)(emission)),
     position = position_dodge2(width = 0.9),
