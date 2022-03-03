86a53a0f20413ad1bb27b1a715308955dc5e6992
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-03-03 16:44:57 +0200

    Refactor calculating lower and upper limits

--- scripts/figures/sensitivity.R
+++ scripts/figures/sensitivity.R
@@ -2,28 +2,32 @@
 library(here)
 library(tidyverse)
 
+add_lower_and_upper_limits <- function(scenario_list, sensitivity_scenario_list, file_name, grouping_variables) {
+  all <- read_and_bind(scenario_list, file_name)
+  sensitivity <- read_and_bind(sensitivity_scenario_list, file_name)
 
-# Data --------------------------------------------------------------------
+  sensitivity <- sensitivity %>%
+    # Values in main scenarios are always inside error bars
+    dplyr::bind_rows(all) %>%
+    dplyr::filter(scenario != sprintf("%i %s", scenarios$year[scenarios$present], scenarios$name[scenarios$present])) %>%
+    dplyr::group_by(across(all_of(grouping_variables))) %>%
+    dplyr::summarise(across(where(is.numeric), list(lower = min, upper = max),
+                            .names = "{.col}_{.fn}"), .groups = "drop")
 
-areas_all <- read_and_bind(scenario_list, "areas")
-areas_sensitivity <- read_and_bind(sensitivity_scenario_list, "areas")
+  all <- all %>%
+    dplyr::left_join(sensitivity, by = grouping_variables) %>%
+    dplyr::mutate(scenario = forcats::as_factor(scenario))
+  return(all)
+}
 
+add_lower_and_upper_limits(scenario_list, sensitivity_scenario_list, "areas", c("scenario", "area")) %>%
+  readr::write_rds(here::here("results", "areas_all.rds"))
 
-# Sensitivity analysis ----------------------------------------------------
+add_lower_and_upper_limits(scenario_list, sensitivity_scenario_list, "emissions", c("scenario", "vehicle")) %>%
+  readr::write_rds(here::here("results", "emissions_all.rds"))
 
-areas_sensitivity <- areas_sensitivity %>%
-  # Values in main scenarios are always inside error bars
-  dplyr::bind_rows(areas_all) %>%
-  dplyr::filter(scenario != sprintf("%i %s", scenarios$year[scenarios$present], scenarios$name[scenarios$present])) %>%
-  dplyr::group_by(scenario, area) %>%
-  dplyr::summarise(across(where(is.numeric), list(lower = min, upper = max),
-                          .names = "{.col}_{.fn}"), .groups = "drop")
+add_lower_and_upper_limits(scenario_list, sensitivity_scenario_list, "vdfs", c("scenario", "vdf")) %>%
+  readr::write_rds(here::here("results", "vdfs_all.rds"))
 
-areas_all <- areas_all %>%
-  dplyr::left_join(areas_sensitivity, by = c("scenario", "area")) %>%
-  dplyr::mutate(scenario = forcats::as_factor(scenario))
-
-
-# Output ------------------------------------------------------------------
-
-readr::write_rds(areas_all, file = here::here("results", "areas_all.rds"))
+add_lower_and_upper_limits(scenario_list, sensitivity_scenario_list, "cargo", c("scenario", "group")) %>%
+  readr::write_rds(here::here("results", "cargo_all.rds"))
