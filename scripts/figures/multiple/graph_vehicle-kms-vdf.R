8ed56ff5f986ac41ea88b4e57feb7a709ea89d37
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-03-03 16:28:07 +0200

    Add error bars

--- scripts/figures/multiple/graph_vehicle-kms-vdf.R
+++ scripts/figures/multiple/graph_vehicle-kms-vdf.R
@@ -12,6 +12,12 @@ results <- readr::read_rds(here::here("results", "vdfs_all.rds"))
 
 ggplot(results, aes(x = vdf, y = share)) +
   geom_col(aes(fill = scenario), position = position_dodge2()) +
+  geom_errorbar(
+    mapping = aes(ymin = share_lower, ymax = share_upper),
+    position =  position_dodge2(width = 0.9, padding = 0.66),
+    color = "#333333",
+    size = 0.35
+  ) +
   geom_text(
     aes(y = share / 2, label = scales::label_percent(accuracy = 1, suffix = "", decimal.mark = ",")(share)),
     position = position_dodge2(width = 0.9),
