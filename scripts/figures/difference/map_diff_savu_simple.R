72ad29429245540122d0994d475cbd3eb632b884
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-03-10 13:43:18 +0200

    Plot SAVU zone difference with only three levels
    better - same - worse

---
+++ scripts/figures/difference/map_diff_savu_simple.R
@@ -0,0 +1,55 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+source(here::here("scripts", "basemap", "functions_map.R"), encoding = "utf-8")
+
+
+# Data --------------------------------------------------------------------
+
+results0 <- readr::read_rds(here::here("results", "zones_2018.rds")) %>%
+  dplyr::select(zone, savu_zone) %>%
+  dplyr::rename(savu_zone0 = savu_zone)
+
+
+results1 <- readr::read_rds(here::here("results", "zones_2040_ve0.rds")) %>%
+  sf::st_drop_geometry() %>%
+  dplyr::select(zone, savu_zone) %>%
+  dplyr::rename(savu_zone1 = savu_zone)
+
+results <- results0 %>%
+  dplyr::left_join(results1, by = "zone") %>%
+  dplyr::mutate(diff_savu_zone = (as.integer(savu_zone0) - as.integer(savu_zone1)))
+
+
+# Plot --------------------------------------------------------------------
+
+label_number_signed <- function(x) {
+  y <- scales::label_number(accuracy = 1)(x)
+  without_sign <- !grepl("-", y)
+  y[without_sign] <- paste0("+", y[without_sign])
+  return(y)
+}
+
+ggplot() +
+  geom_sf(mapping = aes(fill = diff_savu_zone),
+          data = results, color = NA) +
+  scale_fill_gradient2(
+    high = "#3E8606",
+    low = "#7b1154",
+    name = NULL,
+    breaks = -1:1,
+    labels = c("Heikompi SAVU", "Ei muutosta", "Parempi SAVU"),
+    limits = c(-1, 1),
+    guide = "legend",
+    oob = scales::squish
+  ) +
+  geom_basemap() +
+  coord_sf_mal() +
+  annotate_map(
+    title = "Muutos SAVU-vyöhykkeessä",
+    subtitle = "2018 Nykytila \U2192 2040 Vertailupohja"
+  ) +
+  theme_mal_map()
+
+ggsave_map(here::here("figures", "map_diff_savu_simple_2020-2040_ve0.png"))
