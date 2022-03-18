8cbe92c9ac172f4544ae4d11ab4ba7ee53559217
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-03-18 11:40:56 +0200

    Add error bars to malpakka

--- scripts/figures/multiple/graph_malpakka.R
+++ scripts/figures/multiple/graph_malpakka.R
@@ -16,7 +16,7 @@ results_2017 <- results0 %>%
   dplyr::rename(malpakka = tontin_teho_2017)
 
 results <- results0 %>%
-  dplyr::select(scenario, area, malpakka) %>%
+  dplyr::select(scenario, area, malpakka, malpakka_upper, malpakka_lower) %>%
   dplyr::bind_rows(results_2017) %>%
   dplyr::mutate(scenario = factor(scenario, levels = c("2017 Tilasto", levels(results0$scenario)))) %>%
   dplyr::arrange(scenario)
@@ -26,12 +26,12 @@ results <- results0 %>%
 
 ggplot(results, aes(x = area, y = malpakka)) +
   geom_col(aes(fill = scenario), position = position_dodge2()) +
-  # geom_errorbar(
-  #   mapping = aes(ymin = malpakka_lower, ymax = malpakka_upper),
-  #   position =  position_dodge2(width = 0.9, padding = 0.66),
-  #   color = "#333333",
-  #   size = 0.35
-  # ) +
+  geom_errorbar(
+    mapping = aes(ymin = malpakka_lower, ymax = malpakka_upper),
+    position =  position_dodge2(width = 0.9, padding = 0.66),
+    color = "#333333",
+    size = 0.35
+  ) +
   geom_text(
     aes(y = malpakka / 2, label = scales::label_number(accuracy = 0.1, decimal.mark = ",")(malpakka)),
     position = position_dodge2(width = 0.9),
