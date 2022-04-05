53e8ce430c4e471e5aaedbcab075b6d6cbe042ca
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-04-05 13:18:42 +0300

    Add accessibility map

---
+++ scripts/figures/single/map_accessibility.R
@@ -0,0 +1,42 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+library(sf)
+library(viridis)
+source(here::here("scripts", "basemap", "functions_map.R"), encoding = "utf-8")
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", sprintf("zones_%s.rds", scenario_attributes[["scenario"]])))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot() +
+  geom_sf(mapping = aes(fill = accessibility_scaled, color = ""),
+          data = results) +
+  scale_fill_viridis(
+    option = "magma",
+    labels = scales::label_number(accuracy = 1),
+    limits = c(0, 100),
+    direction = -1,
+    na.value = "#ffffff",
+    oob = scales::squish
+  ) +
+  scale_colour_manual(values = NA) +
+  guides(fill = guide_colorbar("indeksi",
+                               order = 1),
+         colour = guide_legend("Liian\nvähän\nasukkaita",
+                               order = 99,
+                               override.aes = list(color = "grey35",
+                                                   fill = "#ffffff"))) +
+  geom_basemap() +
+  coord_sf_mal() +
+  annotate_map(
+    title = "Saavutettavuus asukkaiden näkökulmasta",
+    subtitle = sprintf("%d %s", scenario_attributes[["year"]], scenario_attributes[["name"]])
+  ) +
+  theme_mal_map()
+
+ggsave_map(here::here("figures", sprintf("map_accessibility_%s.png", scenario_attributes[["scenario"]])))
