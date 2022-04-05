469b4ba7472f9001a82c82c604145e8fb9096fa9
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-04-05 13:27:03 +0300

    Create new plots

---
+++ scripts/figures/multiple/graph_dist.R
@@ -0,0 +1,46 @@
+# -*- coding: utf-8-unix -*-
+library(here)
+library(tidyverse)
+
+
+# Data --------------------------------------------------------------------
+
+results <- readr::read_rds(here::here("results", "emissions_all.rds")) %>%
+  dplyr::filter(vehicle != "total")
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(results, aes(x = vehicle, y = dist, fill = scenario)) +
+  geom_col(position = position_dodge2()) +
+  geom_errorbar(
+    mapping = aes(ymin = dist_lower, ymax = dist_upper),
+    position =  position_dodge2(width = 0.9, padding = 0.66),
+    color = "#333333",
+    size = 0.35
+  ) +
+  geom_text(
+    aes(y = dist / 2, label = scales::label_number(accuracy = 0.1, scale = 10^(-6), decimal.mark = ",")(dist)),
+    position = position_dodge2(width = 0.9),
+    size = points2mm(8),
+    color = "#333333"
+  ) +
+  scale_y_continuous(
+    labels = scales::label_number(scale = 10^(-6)),
+    expand = expansion(mult = 0.1)
+  ) +
+  scale_x_discrete(
+    labels = scales::label_wrap(5)
+  ) +
+  scale_fill_manual(
+    name = NULL,
+    values = c("#3E8606", "#7DAD58", "#BFD7AC")
+  ) +
+  labs(
+    title = "Moottoriajoneuvoliikenteen suorite Helsingin seudulla",
+    x =  NULL,
+    y = "milj. ajon.km arkivuorokaudessa"
+  ) +
+  theme_mal_graph()
+
+ggsave_graph(here::here("figures", "graph_dist.png"))
