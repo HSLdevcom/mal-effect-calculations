6436ce2dabb3dd72566fe6191a9d2c48029bcea7
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-04-07 14:03:36 +0300

    Rename column
    readr package was upgraded. New version has different
    naming convention for missing column names.

--- scripts/utils.R
+++ scripts/utils.R
@@ -24,7 +24,7 @@ read_and_bind <- function(scenario_list, prefix, suffix = "rds") {
 read_tsv_helmet <- function(..., first_col_name, comment = "#") {
   withCallingHandlers({
     readr::read_tsv(..., comment = comment) %>%
-      dplyr::rename({{first_col_name}} := X1) %>%
+      dplyr::rename({{first_col_name}} := `...1`) %>%
       dplyr::rename_with(~ gsub("-", "_", .x, fixed = TRUE))
   }, warning = function(w) {
     # Helmet results never include first column name
