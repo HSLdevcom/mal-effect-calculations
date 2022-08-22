8c030fafc2ed974537b53f256461535be20faaaf
Johanna Piipponen <johanna.piipponen@hsl.fi>
2022-08-22 16:55:54 +0300

    Plot apartments, not square footage (kerros-m2)

--- scripts/figures/single/map_ensi-kem2.R
+++ scripts/figures/single/map_ensi-kem2.R
@@ -9,7 +9,7 @@ library(ggnewscale)
 # Data --------------------------------------------------------------------
 
 results <- readr::read_rds(here::here("results", "squares.rds")) %>%
-  dplyr::mutate(floor_area_diff = dplyr::if_else(!ensi, -floor_area_increase_2021_2040_ve0, floor_area_increase_2021_2040_ve0))
+  dplyr::mutate(apartments_diff = dplyr::if_else(!ensi, -apartments_diff_2022_2035, apartments_diff_2022_2035))
 
 ensi <- readr::read_rds(here::here("results", "ensi.rds"))
 
@@ -27,7 +27,7 @@ df_value <- data.frame(
 category_colors <- RColorBrewer::brewer.pal(5, "PiYG")[c(5, 1)]
 
 ggplot() +
-  geom_sf(mapping = aes(fill = floor_area_diff),
+  geom_sf(mapping = aes(fill = apartments_diff),
           data = results, color = NA) +
   geom_basemap() +
   geom_sf(mapping = aes(),
@@ -35,10 +35,10 @@ ggplot() +
   scale_fill_fermenter(
     palette = "PiYG",
     direction = 1,
-    name = "kerros-m2",
+    name = "asuntoa",
     labels = scales::label_number(accuracy = 1),
-    breaks = c(-5000, -1000, -250, -5, 5, 250, 1000, 5000),
-    limits = c(-10000, 10000),
+    breaks = c(-250, -100, -25, -5, 5, 25, 100, 250),
+    limits = c(-500, 500),
     oob = scales::squish,
     guide = guide_none()
   ) +
@@ -47,9 +47,9 @@ ggplot() +
   scale_fill_fermenter(
     palette = "Greys",
     direction = 1,
-    name = "kerros-m2",
-    limits = c(0, 10000),
-    breaks = c(5, 250, 1000, 5000),
+    name = "asuntoa",
+    limits = c(0, 500),
+    breaks = c(5, 25, 100, 250),
     guide = guide_colorsteps(order = 1)
   ) +
   geom_line(data = df_value, aes(x = x, y = y, color = category), group = 1, key_glyph = draw_key_rect) +
@@ -61,11 +61,11 @@ ggplot() +
   ) +
   coord_sf_mal() +
   annotate_map(
-    title = "Uudet asuinkerrosneliöt ensisijaisesti kehitettävillä vyöhykkeillä",
-    subtitle = "2040 Vertailupohja"
+    title = "Ennustettu asuntotuotanto ensisijaisilla vyöhykkeillä",
+    subtitle = NULL
   ) +
   theme_mal_map() +
   theme(legend.box = "horizontal",
         legend.box.just = "top")
 
-ggsave_map(here::here("figures", "map_ensi-kem2_2040_ve0.png"))
+ggsave_map(here::here("figures", "map_ensi_apartments.png"))
